f8103b65ec2390e2ae6d8882bbe308b6bb9281f1
Bepa88 <leleiko.vira@gmail.com>
2020-01-08 17:03:24 +0200

    Add file

---
+++ ChangeFilesDate/ChangeFilesDate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CommandLine;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeFilesDate
+{
+    class ChangeFilesDate
+    {
+        static void Main(string[] args)
+        {
+            //string path = @"C:\Videos";
+            //DateTime newDate = new DateTime(2019, 09, 27);
+
+            var options = new SomeOptions();
+            var v = Parser.Default.ParseArguments<SomeOptions>(args)
+    .WithParsed<SomeOptions>(opts => options = opts);
+
+            DirectoryInfo di = new DirectoryInfo(options.Path);
+            FileInfo[] images = di.GetFiles(options.Filter);
+            foreach (var image in images)
+            {
+                if (options.Date == "not specified")
+                {
+                    options.Date = DateTime.Now.ToString();
+                }
+
+                image.CreationTime = Convert.ToDateTime(options.Date);
+            }
+        }
+    }
+}
---
+++ ChangeFilesDate/ChangeFilesDate/SomeOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CommandLine;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeFilesDate
+{
+    class SomeOptions
+    {
+        [Option('p', "path", Default = ".")] //"$(pwd)" cd
+        public string Path { get; set; }
+
+        [Option('f', "filter", Default = "*")]
+        public string Filter { get; set; }
+
+        [Option('d', "date", Default = "not specified")]
+        public string Date { get; set; }
+    }
+}
